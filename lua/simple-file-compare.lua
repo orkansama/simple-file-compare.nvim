07bd85eed117c6c656bb73bead6a50be355d928c
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-02 15:48:16 +0200

    testcode

--- lua/simple-file-compare.lua
+++ lua/simple-file-compare.lua
@@ -1,23 +1,76 @@
-print("loaded git-branch-selector.nvim")
-
 local M = {}
 
-M.setup = function()
-	SaveAllBranches()
+local modes = {
+	vimUiSelect = "vimUiSelect",
+	snacks = "snacks",
+	miniPick = "miniPick",
+}
+
+local config = {
+	mode = modes.snacks,
+}
+
+M.setup = function(user_config)
+	user_config = user_config or {}
+	config = vim.tbl_deep_extend("force", config, user_config) -- merge both to be config, but with the right values
+
+	if not modes[config.mode] then
+		error("Invalid mode")
+	end
+
+	vim.api.nvim_create_user_command("Test", function()
+		Compare()
+	end, {})
 end
 
-function SaveAllBranches()
-	local outputTable = vim.fn.systemlist("git branch -r")
-	-- for _, singleValue in ipairs(outputString) do
-	-- print(singleValue)
-	-- end
+function Compare()
+	local outputTable = vim.fn.systemlist("git branch")
 
-	vim.ui.select(outputTable, {
-		prompt = "Select an Element",
-	}, function(choice)
-		if choice then
-			print("Picked:" .. choice)
-		end
-	end)
+	if config.mode == modes.snacks then
+		local snacks = require("snacks")
+
+		snacks.picker.select(outputTable, {
+			prompt = "Select Branch",
+		}, function(choice)
+			ChoiceLogic(choice)
+		end)
+	end
+
+	if config.mode == modes.vimUiSelect then
+		vim.ui.select(outputTable, {
+			prompt = "Select Branch",
+		}, function(choice)
+			if choice then
+				ChoiceLogic(choice)
+			end
+		end)
+	end
+
+	if config.mode == modes.miniPick then
+	end
 end
+
+		vim.cmd("only")
+		vim.api.nvim_set_current_buf(bufferPathToReturn)
+function ChoiceLogic(choice)
+	local selectedActiveBranch = vim.startswith(choice, "*")
+	if selectedActiveBranch then
+		return vim.notify("Cant compare with active branch!", 4)
+	else
+		local currentFile = vim.fn.bufname("%")
+		local otherContent = vim.fn.systemlist("git show " .. choice .. ":" .. currentFile)
+		local bufferPathToReturn = vim.api.nvim_get_current_buf()
+
+		vim.cmd("vsp | enew")
+		local newBuf = vim.api.nvim_get_current_buf()
+		vim.api.nvim_buf_set_lines(newBuf, 0, -1, false, otherContent)
+
+		vim.cmd("diffthis")
+		vim.cmd("wincmd p")
+		vim.cmd("diffthis")
+
+		ReturnToBuffer(bufferPathToReturn)
+	end
+end
+
 return M

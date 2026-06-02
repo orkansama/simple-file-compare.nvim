9150fe8a11d06d03a0267207eecb477cfe935b5f
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-02 18:17:12 +0000

    refactor: Made functions local

--- lua/simple-file-compare.lua
+++ lua/simple-file-compare.lua
@@ -5,37 +5,34 @@ local modes = {
 	snacks = "snacks",
 	miniPick = "miniPick",
 }
-
 local config = {
 	mode = modes.snacks,
 }
 
+local Compare
+local ChoiceLogic
+local ReturnToBuffer
+
 M.setup = function(user_config)
 	user_config = user_config or {}
-	config = vim.tbl_deep_extend("force", config, user_config) -- merge both to be config, but with the correct values
-
+	config = vim.tbl_deep_extend("force", config, user_config)
 	if not modes[config.mode] then
 		error("Invalid mode")
 	end
-
 	vim.api.nvim_create_user_command("FileCompareOpen", function()
 		Compare()
 	end, {})
 end
-
-function Compare()
+Compare = function()
 	local outputTable = vim.fn.systemlist("git branch")
-
 	if config.mode == modes.snacks then
 		local snacks = require("snacks")
-
 		snacks.picker.select(outputTable, {
 			prompt = "Select Branch",
 		}, function(choice)
 			ChoiceLogic(choice)
 		end)
 	end
-
 	if config.mode == modes.vimUiSelect then
 		vim.ui.select(outputTable, {
 			prompt = "Select Branch",
@@ -45,7 +42,6 @@ function Compare()
 			end
 		end)
 	end
-
 	if config.mode == modes.miniPick then
 		local miniPick = require("mini.pick")
 		miniPick.setup()
@@ -58,8 +54,7 @@ function Compare()
 		ChoiceLogic(choice)
 	end
 end
-
-function ReturnToBuffer(bufferPathToReturn)
+ReturnToBuffer = function(bufferPathToReturn)
 	vim.api.nvim_create_user_command("FileCompareClose", function()
 		vim.cmd("only")
 		for _, buf in ipairs(vim.api.nvim_list_bufs()) do
@@ -70,8 +65,7 @@ function ReturnToBuffer(bufferPathToReturn)
 		vim.api.nvim_set_current_buf(bufferPathToReturn)
 	end, {})
 end
-
-function ChoiceLogic(choice)
+ChoiceLogic = function(choice)
 	local selectedActiveBranch = vim.startswith(choice, "*")
 	if selectedActiveBranch then
 		return vim.notify("Cant compare with active branch!", 4)
@@ -79,15 +73,12 @@ function ChoiceLogic(choice)
 		local currentFile = vim.fs.normalize(vim.fn.bufname("%"))
 		local otherContent = vim.fn.systemlist("git show " .. choice .. ":" .. currentFile)
 		local bufferPathToReturn = vim.api.nvim_get_current_buf()
-
 		vim.cmd("vsp | enew")
 		local newBuf = vim.api.nvim_get_current_buf()
 		vim.api.nvim_buf_set_lines(newBuf, 0, -1, false, otherContent)
-
 		vim.cmd("diffthis")
 		vim.cmd("wincmd p")
 		vim.cmd("diffthis")
-
 		ReturnToBuffer(bufferPathToReturn)
 	end
 end
